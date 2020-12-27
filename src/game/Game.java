c1a2b60f80e9f2f618f31e1a8ae565d3722ee40a
Leyu Xu <leyuxu@usc.edu>
2020-12-26 18:21:53 -0800

    start on file I/O

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,10 +1,37 @@
 package game;
 
+import java.io.*;
+import com.google.gson.Gson;
+
 public class Game {
+	Scene mCurrentScene;
+	
+	//load a scene from a specific gson file
+	public void LoadSceneFromJson(String filename) throws FileNotFoundException
+	{
+		//create reader object and read
+		Gson gson = new Gson();
+		Reader fReader = new FileReader(filename);
+		mCurrentScene = gson.fromJson(fReader, Scene.class);
+	}
+	
+	//test json file I/O
+	public void TestIO(String filename) throws FileNotFoundException
+	{
+		LoadSceneFromJson(filename);
+		System.out.println(mCurrentScene.toString());
+	}
 
 	public static void main(String[] args) {
+		//instantiate a game
+		Game game = new Game();
 		
-
+		//ONLY for testing I/O
+		try {
+			game.TestIO("testJson/test1.json");
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
+		}
 	}
 
 }
---
+++ src/game/Scene.java
@@ -0,0 +1,31 @@
+package game;
+
+public class Scene {
+	protected String mDescription;
+	protected String[] mOptions;
+	protected String[] mOptionReferences;
+	
+	@Override
+	public String toString()
+	{
+		StringBuffer buffer = new StringBuffer();
+		
+		//append scene description
+		buffer.append("Scene Description: ");
+		buffer.append(mDescription + "\n");
+		
+		//append options
+		for(String each_str : mOptions)
+		{
+			buffer.append("Option: " + each_str + "\n");
+		}
+		
+		//append references
+		for(String each_str: mOptionReferences)
+		{
+			buffer.append("Option Reference: " + each_str + "\n");
+		}
+		
+		return buffer.toString();
+	}
+}
